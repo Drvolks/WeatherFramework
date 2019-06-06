8deda63f03a7c5f1f66922ecbc096c0942a735a6
Jean-François Dufour <jfdufour@me.com>
2019-06-06 18:38:51 -0400

    Ajout de tests rss

---
+++ WeatherFrameworkTests/TestUtils.swift
@@ -0,0 +1,23 @@
+//
+//  TestUtils.swift
+//  weatherlr
+//
+//  Created by Jean-Francois Dufour on 2016-04-21.
+//  Copyright © 2016 Jean-Francois Dufour. All rights reserved.
+//
+
+import Foundation
+
+class TestUtils {
+    static func iterateEnum<T: Hashable>(_: T.Type) -> AnyIterator<T> {
+        var i = 0
+        return AnyIterator {
+            let next = withUnsafePointer(to: &i) {
+                $0.withMemoryRebound(to: T.self, capacity: 1) { $0.pointee }
+            }
+            if next.hashValue != i { return nil }
+            i += 1
+            return next
+        }
+    }
+}
